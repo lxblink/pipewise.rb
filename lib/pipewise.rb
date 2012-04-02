2b077d52a36a391da463e6a44eff6a8319752596
Jeff Watts <jeff.watts@pipewise.com>
2012-04-02 08:38:24 -0700

    Added RDoc comments for pipewise.rb

--- lib/pipewise.rb
+++ lib/pipewise.rb
@@ -3,6 +3,9 @@ require 'json'
 require 'pipewise/errors'
 
 class Pipewise
+  # Constructor takes the Pipewise API key (required), and optionally
+  # takes a hash which allows you to specify :insecure => true
+  # for use of HTTP instead of HTTPS. HTTPS is the default.
   def initialize(api_key, options = {})
     @api_key = api_key
     @host = options[:host] || 'api.pipewise.com'
@@ -11,10 +14,22 @@ class Pipewise
 
   attr_reader :api_key, :host, :protocol
 
+  # Sends user info to Pipewise for tracking the user identifed by the given email 
+  # address (required), and supports an optional hash of properties for that user.
+  # Pipewise will update the given user's properties each time this method is called
+  # and will record a sighting event for this user, which updates the last active 
+  # timestamp for this user. If you do NOT want Pipewise to record a sighting event
+  # for this user, then add :pw_no_event => true to the property hash. Each call to
+  # this method will make a blocking HTTP request.
   def track_user(email, user_properties = {})
     post_request('track', {:email => email}.merge(user_properties))
   end
 
+  # Sends event info to Pipewise for the user identifed by the given email 
+  # address (required) and records an event of the given type (required). This method 
+  # also supports an optional hash of properties describing this event. A new event 
+  # will be recorded every time this method is called, and the user's last active 
+  # timestamp will be updated. Each call to this method will make a blocking HTTP request.
   def track_event(user_email, event_type, event_properties = {})
     post_request('track_event', {:email => user_email, :type => event_type}.merge(event_properties))
   end
